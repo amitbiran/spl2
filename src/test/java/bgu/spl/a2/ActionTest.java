30b0cda6af9ff9dac2bb531fd75bace4c0ae22f9
amit <amitbiran2@gmail.com>
2017-12-09 16:12:44 +0200

    finished part one with banks test

--- src/test/java/bgu/spl/a2/ActionTest.java
+++ src/test/java/bgu/spl/a2/ActionTest.java
@@ -11,7 +11,7 @@ class simpleAction extends Action{
 
     public simpleAction(String text){
         this.text = text;
-        result = new Promise();
+        result = new Promise<Integer>();
     }
 
 
@@ -55,21 +55,39 @@ public class ActionTest extends TestCase {
     }
 
     public void testThen() throws Exception {
+        ActorThreadPool pool =new ActorThreadPool(5);
+        String actorId = "actor1";
+        PrivateState actorPrivateState = new PrivateState() {};
+        Action a = new simpleAction("test 1 handle");
+        Action b = new simpleAction("test 11 handle");
+        pool.start();
+        pool.submit(a,actorId,actorPrivateState);
+        pool.submit(b,actorId,actorPrivateState);
     }
 
     public void testComplete() throws Exception {
+        Action a = new simpleAction("test 1 handle");
+        a.complete(new Integer(8));
+      System.out.println(a.getResult().get());
     }
 
     public void testGetResult() throws Exception {
+        Action a = new simpleAction("test 1 handle");
+        System.out.println(a.getResult());
     }
 
     public void testSendMessage() throws Exception {
+        testHandle();
     }
 
     public void testSetActionName() throws Exception {
+        Action a = new simpleAction("test 1 handle");
+        a.setActionName("action number 1");
+        System.out.println(a.getActionName());
     }
 
     public void testGetActionName() throws Exception {
+        testSetActionName();
     }
 
 }
--- src/test/java/bgu/spl/a2/banksTest.java
+++ src/test/java/bgu/spl/a2/banksTest.java
@@ -1,7 +1,94 @@
 package bgu.spl.a2;
 
+import org.omg.CORBA.VersionSpecHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Lenovo on 12/9/2017.
  */
+class confirmation extends Action{
+    String clientA;
+    String clientB;
+    String bankB;
+    PrivateState bankState;
+    public confirmation(String clientA,String clientB,String bankB,PrivateState bankState){
+    this.clientA = clientA;
+    this.clientB =clientB;
+    this.bankB =bankB;
+    this.bankState =bankState;
+    result = new Promise<Boolean>();
+    }
+    @Override
+    protected void start() {
+        result.resolve(true);//just for test lets say the other bank always approve the transaction
+    }
+}
+
+
+
+class Transmission extends Action{
+    int amount;
+    String clientA;
+    String clientB;
+    String bankA;
+    String bankB;
+    PrivateState bankState;
+    VersionMonitor vm ;
+    public Transmission(int amount,String clientA,String clientB,String bankA,String bankB,PrivateState bankState){
+        this.amount = amount;
+        this.clientA =clientA;
+        this.clientB = clientB;
+        this.bankA =bankA;
+        this.bankB =bankB;
+        this.bankState =bankState;
+        vm = new VersionMonitor();
+        result = new Promise<String>();
+    }
+
+    public VersionMonitor getVm(){
+        return vm;
+    }
+    protected void start(){
+        List<Action<Boolean>> actions = new ArrayList<>();
+        Action<Boolean> confAction = new confirmation(clientA,clientB,bankB,new PrivateState() {});
+        actions.add(confAction);
+        sendMessage(confAction, bankB, new PrivateState() {});
+        then(actions,()->{
+            Boolean result = actions.get(0).getResult().get();
+            if(result==true){
+                complete("transmission good");
+                System.out.println("transmission good");
+                vm.inc();
+            }
+            else{
+                complete("transmission bad");
+                System.out.println("transmission bad");
+            }
+        });
+
+    }
+
+}
+
 public class banksTest {
+
+    public static void main(String[] args){
+        ActorThreadPool pool = new ActorThreadPool(8);
+        Action<String> trans = new Transmission(100,"A","B","bank1","bank2",new PrivateState(){});
+        pool.start();
+        pool.submit(trans,"bank1",new PrivateState() {});
+        VersionMonitor l= new VersionMonitor();
+        try {
+            ((Transmission) trans).getVm().await(0);
+            pool.shutdown();
+        }
+catch(Exception e){
+
+        }
+
+
+    }
+
 }
